cc7c21a72a105b782d175df3fe535c29b59131b7
EasternOwo <eastern.wolph@gmail.com>
2025-12-26 21:02:23 +0800

    Update the appearance of links

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const links = document.querySelectorAll('.social-link');
-const title = document.getElementById('social-link-title');
+// const links = document.querySelectorAll('.social-link');
+// const title = document.getElementById('social-link-title');
 
-links.forEach(link => {
-    link.addEventListener('mouseenter', () => {
-        title.textContent = link.getAttribute('data-link-title');
-    });
+// links.forEach(link => {
+//     link.addEventListener('mouseenter', () => {
+//         title.textContent = link.getAttribute('data-link-title');
+//     });
 
-    link.addEventListener('mouseleave', () => {
-        title.textContent = '';
-    });
-});
+//     link.addEventListener('mouseleave', () => {
+//         title.textContent = '';
+//     });
+// });
+
+const footnote = document.querySelector('footer');
+const year = new Date().getFullYear();
+
+footnote.textContent = `© ${year} 東`
