b813f9cce438666ccde4e68e4ce8797ea7a4e57e
EasternOwo <eastern.wolph@gmail.com>
2026-02-13 19:00:59 +0800

    Update text, adjust character list display threshold

--- script.js
+++ script.js
@@ -16,14 +16,19 @@ const year = new Date().getFullYear();
 
 footnote.textContent = `© ${year} 東`
 
+let copyTimeout;
+
 function copyText() {
   var text = "eastern.owo";
 
   navigator.clipboard.writeText(text).then(() => {
     const element = document.getElementById('discord-link-text');
     element.textContent = '已複製 ID 到剪貼簿：eastern.owo';
-    setTimeout(() => {
-      element.textContent = '可以在這裡跟我說話';
+  
+    clearTimeout(copyTimeout);
+
+    copyTimeout = setTimeout(() => {
+      element.textContent = '可以在這裡找到我';
     }, 2000);
   }).catch(err => {
     console.error('Could not copy text: ', err);
